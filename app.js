7b5ed395b6b61a5b04e14f6faa7c0c296f4b9384
jpreslov <lovett_jp@outlook.com>
2021-09-29 15:28:38 -0500

    lets get it

---
+++ app.js
@@ -0,0 +1,49 @@
+// name = "Thim";
+// var name;
+// console.log(name);
+
+// setName();
+// function setName() {
+//   var name = "Covalence";
+//   console.log(name);
+// }
+
+// console.log("first");
+// let avg = findAvg(2, 2);
+// console.log("second", avg);
+
+// function findAvg(a, b) {
+//   console.log("third");
+//   var answer = (a + b) / 2;
+//   return answer;
+// }
+
+// let fruits = ["apples", "tomatoes", "pineapple"];
+
+// let favFruit;
+
+// function printFruits() {
+//   favFruit = fruits[2];
+//   console.log(fruits[0]);
+
+//   function printFavFruit() {
+//     console.log(favFruit);
+
+//     let leastFav = 'cantaloupe'
+//   }
+//   console.log(leastFav)
+
+//   printFavFruit();
+// }
+
+// printFruits();
+
+// someFunc();
+// function someFunc() {
+//     console.log("hello, whitney")
+// }
+
+// let exampleFunc = function(){
+//     alert("wat popping!!!!!!!!!!!!")
+// }
+// exampleFunc()
